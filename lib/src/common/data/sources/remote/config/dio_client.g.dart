02c837dcf95880ca9eb578853a1152345f275c97
Adithya Firmansyah Putra <aditputrafirmansyah@gmail.com>
2026-03-21 21:51:49 +0700

    chore(backend): commit generated dart files for NIB-8

--- lib/src/common/data/sources/remote/config/dio_client.g.dart
+++ lib/src/common/data/sources/remote/config/dio_client.g.dart
@@ -6,19 +6,22 @@ part of 'dio_client.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$dioHash() => r'0000000000000000000000000000000000000000';
+String _$dioHash() => r'33dcf94ce4b20117125003d9e9bb543a95473b0a';
 
 /// See also [dio].
 @ProviderFor(dio)
 final dioProvider = Provider<Dio>.internal(
   dio,
   name: r'dioProvider',
-  debugGetCreateSourceHash:
-      const bool.fromEnvironment('dart.vm.product') ? null : _$dioHash,
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$dioHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
 
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
 typedef DioRef = ProviderRef<Dio>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
--- lib/src/common/services/local_flag_service.g.dart
+++ lib/src/common/services/local_flag_service.g.dart
@@ -6,21 +6,22 @@ part of 'local_flag_service.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$localFlagServiceHash() => r'0000000000000000000000000000000000000000';
+String _$localFlagServiceHash() => r'1ecb3d155d31c9f9eb11d96f9013b1a7eeddd020';
 
 /// See also [localFlagService].
 @ProviderFor(localFlagService)
 final localFlagServiceProvider = AutoDisposeProvider<LocalFlagService>.internal(
   localFlagService,
   name: r'localFlagServiceProvider',
-  debugGetCreateSourceHash:
-      const bool.fromEnvironment('dart.vm.product')
-          ? null
-          : _$localFlagServiceHash,
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$localFlagServiceHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
 
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
 typedef LocalFlagServiceRef = AutoDisposeProviderRef<LocalFlagService>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
