3ef7ffbf6fbb2384bad68137d248011222538c17
Adithya Firmansyah Putra <aditputrafirmansyah@gmail.com>
2026-04-08 10:58:01 +0800

    fix(infra): move hardcoded Firebase keys to env vars
    Removes all hardcoded Firebase API keys from firebase_options.dart and
    loads them via flutter_dotenv through FlavorConfig at runtime.
    
    Closes #36

--- lib/src/app/config/flavor_config.dart
+++ lib/src/app/config/flavor_config.dart
@@ -7,6 +7,14 @@ class FlavorConfig {
     required this.supabaseAnonKey,
     required this.revenueCatAppleKey,
     required this.revenueCatGoogleKey,
+    required this.firebaseAndroidApiKey,
+    required this.firebaseAndroidAppId,
+    required this.firebaseIosApiKey,
+    required this.firebaseIosAppId,
+    required this.firebaseMessagingSenderId,
+    required this.firebaseProjectId,
+    required this.firebaseStorageBucket,
+    required this.firebaseIosBundleId,
   });
 
   static late FlavorConfig instance;
@@ -16,6 +24,14 @@ class FlavorConfig {
   final String supabaseAnonKey;
   final String revenueCatAppleKey;
   final String revenueCatGoogleKey;
+  final String firebaseAndroidApiKey;
+  final String firebaseAndroidAppId;
+  final String firebaseIosApiKey;
+  final String firebaseIosAppId;
+  final String firebaseMessagingSenderId;
+  final String firebaseProjectId;
+  final String firebaseStorageBucket;
+  final String firebaseIosBundleId;
 
   bool get isDev => flavor == Flavor.dev;
   bool get isProd => flavor == Flavor.prod;
@@ -28,6 +44,14 @@ class FlavorConfig {
     required String supabaseAnonKey,
     required String revenueCatAppleKey,
     required String revenueCatGoogleKey,
+    required String firebaseAndroidApiKey,
+    required String firebaseAndroidAppId,
+    required String firebaseIosApiKey,
+    required String firebaseIosAppId,
+    required String firebaseMessagingSenderId,
+    required String firebaseProjectId,
+    required String firebaseStorageBucket,
+    required String firebaseIosBundleId,
   }) {
     instance = FlavorConfig._(
       flavor: flavor,
@@ -35,6 +59,14 @@ class FlavorConfig {
       supabaseAnonKey: supabaseAnonKey,
       revenueCatAppleKey: revenueCatAppleKey,
       revenueCatGoogleKey: revenueCatGoogleKey,
+      firebaseAndroidApiKey: firebaseAndroidApiKey,
+      firebaseAndroidAppId: firebaseAndroidAppId,
+      firebaseIosApiKey: firebaseIosApiKey,
+      firebaseIosAppId: firebaseIosAppId,
+      firebaseMessagingSenderId: firebaseMessagingSenderId,
+      firebaseProjectId: firebaseProjectId,
+      firebaseStorageBucket: firebaseStorageBucket,
+      firebaseIosBundleId: firebaseIosBundleId,
     );
   }
 }
--- lib/src/app/runner.dart
+++ lib/src/app/runner.dart
@@ -25,6 +25,14 @@ Future<void> bootstrap({required Flavor flavor}) async {
     supabaseAnonKey: dotenv.env['SUPABASE_ANON_KEY']!,
     revenueCatAppleKey: dotenv.env['REVENUECAT_APPLE_KEY']!,
     revenueCatGoogleKey: dotenv.env['REVENUECAT_GOOGLE_KEY']!,
+    firebaseAndroidApiKey: dotenv.env['FIREBASE_ANDROID_API_KEY']!,
+    firebaseAndroidAppId: dotenv.env['FIREBASE_ANDROID_APP_ID']!,
+    firebaseIosApiKey: dotenv.env['FIREBASE_IOS_API_KEY']!,
+    firebaseIosAppId: dotenv.env['FIREBASE_IOS_APP_ID']!,
+    firebaseMessagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID']!,
+    firebaseProjectId: dotenv.env['FIREBASE_PROJECT_ID']!,
+    firebaseStorageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET']!,
+    firebaseIosBundleId: dotenv.env['FIREBASE_IOS_BUNDLE_ID']!,
   );
 
   // 3. Hive init + open boxes (wired in NIB-8)
